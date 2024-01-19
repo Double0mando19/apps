2dc421c3559a90e4e4d87f8b114fb0addd12d78b
Double0mando19 <129148640+Double0mando19@users.noreply.github.com>
2024-01-19 12:14:20 -0500

    Add files via upload

---
+++ BirthStoneApp copy.py
@@ -0,0 +1,47 @@
+try:
+    month_of_birth = input("Enter month of birth.?\n ")
+
+    if month_of_birth == "January":
+        print(f"Your birth stone is Garnet")
+    if month_of_birth == "February":
+        print(f"Your birth stone is Amethyst")
+    if month_of_birth == "March":
+        print(f"Your birth stone is Aquamarine")
+    if month_of_birth == "April":
+        print(f"Your birth stone is Diamond")
+    if month_of_birth == "May":
+        print(f"Your birth stone is Emerald")
+    if month_of_birth == "June":
+        print(f"Your birth stone is Alexandrite")
+    if month_of_birth == "July":
+        print(f"Your birth stone is Rudy")
+    if month_of_birth == "August":
+        print(f"Your birth stone is Peridot")
+    if month_of_birth == "September":
+        print(f"Your birth stone is Sapphire")
+    if month_of_birth == "October":
+        print(f"Your birth stone is Tourmaline")
+    if month_of_birth == "November":
+        print(f"Your birth stone is Topaz")
+    if month_of_birth == "December":
+        print(f"Your birth stone is Blue Topaz")
+except:
+    print("Invalid text try again.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
